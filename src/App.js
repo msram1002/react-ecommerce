55989e9302e8bca6950f5cc01f02935d1ee00a00
Sundara Ram Matta <msram0210@gmail.com>
2021-09-23 10:24:48 -0400

    better way for components import

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Products, Navbar } from './components';
 function App() {
   return (
     <div>
+      <Navbar />
       <Products />
     </div>
   )
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { AppBar, ToolBar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
 
-import logo from '../assets/e-commerce.png'
+import logo from '../../assets/e-commerce.png';
 import useStyles from './styles';
 
 const Navbar = () => {
@@ -10,9 +10,9 @@ const Navbar = () => {
   return (
     <>
       <AppBar position="fixed" className={classes.AppBar} color="inherit">
-        <ToolBar>
+        <Toolbar>
           <Typography variant="h6" className={classes.title} color="inherit">
-            <img src={logo} alt="E-commerce" height="25px" className={classes.image} />
+            <img src={logo} alt="ecommerce" height="60px" className={classes.image} />
               SRM E-Commerce Store
           </Typography>
           <div className={classes.grow}/>
@@ -23,7 +23,7 @@ const Navbar = () => {
               </Badge>
             </IconButton>
           </div>
-        </ToolBar>
+        </Toolbar>
       </AppBar>
     </>
   )
