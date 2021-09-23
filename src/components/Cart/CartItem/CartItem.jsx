83c39702a5e34186fda732932001ad98d5e724af
Sundara Ram Matta <msram0210@gmail.com>
2021-09-23 14:27:18 -0400

    creating cart item component

---
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Typography, Button, Card, CardActions, CardContent, CardMedia } from '@material-ui/core';
+
+import useStyles from './styles';
+
+const CartItem = () => {
+  const classes = useStyles();
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CartItem
