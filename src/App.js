2c56d9eace71e26a20ca4b05648ab5c744d06bce
Sundara Ram Matta <msram0210@gmail.com>
2021-09-23 13:59:34 -0400

    fetching products list and adding logic for cart update

--- src/App.js
+++ src/App.js
@@ -5,21 +5,35 @@ import { Products, Navbar } from './components';
 import { commerce } from './lib/commerce';
 
 function App() {
+  // For the products
   const [products, setProducts] = useState([]);
+
+  // Set the initial cart count and update as user add to cart
+  const [cart, setCart] = useState({});
 
   const fetchProd = async () => {
     const { data } = await commerce.products.list();
     setProducts(data);
   };
 
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+  }
+
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+    setCart(item.cart);
+  }
+
   useEffect(() => {
     fetchProd();
+    fetchCart();
   }, []);
 
   return (
     <div>
-      <Navbar />
-      <Products products={products} />
+      <Navbar totalItems={cart.total_items}/>
+      <Products products={products} onAddToCart= {handleAddToCart} />
     </div>
   )
 }
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -9,7 +9,7 @@ import useStyles from './styles';
 //   { id: 2, name: "Mac Book", description: "Apple Laptop", price: "$10" },
 // ];
 
-const Products = ({ products }) => {
+const Products = ({ products, onAddToCart }) => {
   const classes = useStyles();
   return (  
     <main className={classes.content}>
@@ -17,7 +17,7 @@ const Products = ({ products }) => {
       <Grid container justify="center" spacing={4}>
         {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-            <Product product={product} />
+            <Product product={product} onAddToCart={onAddToCart} />
           </Grid>
         ))}
       </Grid>
